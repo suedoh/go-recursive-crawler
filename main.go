0fb20f65d6d582ed60e3409d1afebca623419c3d
suedoh <suedoh36@proton.me>
2023-05-02 21:12:23 -0400

    updated imports and created interruptHandler

--- main.go
+++ main.go
@@ -1,64 +1,230 @@
 package main
 
 import (
-	"fmt"
-	"net/url"
-	"os"
-	"sync"
-	"time"
+    "net/http"
+    "os"
+    "os/signal"
+    "path"
+    "path/filepath"
+    "sync"
+    "bytes"
+    "fmt"
+    "io"
+    "net/url"
+    "flag"
+    "log"
+    "strings"
+    "io/ioutil"
 
-	"github.com/suedoh/go-recursive-crawler"
+    "golang.org/x/net/html"
+)
+
+type URLData struct {
+    URL          *url.URL
+    Filename     string
+    Downloaded   bool
+    RelativePath string
+    Depth        int
+}
+
+type Spider struct {
+    RootURL     string
+    OutputDir   string
+    Concurrency int
+    MaxDepth    int
+    MaxURLs     int
+    domain      string
+    urlsVisited int
+    urls        chan URLData
+}
+
+var client = http.Client{}
+var (
+    mutex sync.Mutex
+    wg    sync.WaitGroup
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Printf("Usage: %s <url> <directory>\n", os.Args[0])
-		os.Exit(1)
-	}
+    // Parse command line arguments
+    flag.Parse()
 
-	startURL := os.Args[1]
-	destDir := os.Args[2]
+    // Check if starting URL is valid
+    _, err := url.ParseRequestURI(*startURL)
+    if err != nil {
+        log.Fatalf("Invalid starting URL: %s", err)
+    }
 
-	// Parse the start URL
-	parsedURL, err := url.Parse(startURL)
-	if err != nil {
-		fmt.Printf("Error parsing URL: %v\n", err)
-		os.Exit(1)
-	}
+    // Create destination directory if it doesn't exist
+    if _, err := os.Stat(*destDir); os.IsNotExist(err) {
+        err := os.MkdirAll(*destDir, os.ModePerm)
+        if err != nil {
+            log.Fatalf("Error creating destination directory: %s", err)
+        }
+    }
 
-	// Create the destination directory if it doesn't exist
-	err = os.MkdirAll(destDir, os.ModePerm)
-	if err != nil {
-		fmt.Printf("Error creating directory: %v\n", err)
-		os.Exit(1)
-	}
+    // Create a channel to receive URLs to crawl
+    toCrawl := make(chan string)
 
-	// Create a map to store files that have already been downloaded
-	downloaded := make(map[string]bool)
+    // Create a channel to receive URLs to skip
+    toSkip := make(chan string)
 
-	// Create a channel for file data and a wait group for handling concurrency
-	fileDataChan := make(chan webcrawler.FileData)
-	var wg sync.WaitGroup
+    // Create a channel to signal when all URLs have been crawled
+    done := make(chan bool)
 
-	// Kick off the crawl
-	wg.Add(1)
-	go webcrawler.Crawl(parsedURL, destDir, downloaded, fileDataChan, &wg)
+    // Create a crawl manager with the specified options
+    c := CrawlManager{
+        StartURL: *startURL,
+        DestDir:  *destDir,
+        MaxDepth: *maxDepth,
+        MaxPages: *maxPages,
+        ToCrawl:  toCrawl,
+        ToSkip:   toSkip,
+        Done:     done,
+    }
 
-	// Wait for all the crawling to complete
-	go func() {
-		wg.Wait()
-		close(fileDataChan)
-	}()
+    // Start the crawl manager
+    go c.Start()
 
-	// Loop through the file data channel and save the files to disk
-	for fileData := range fileDataChan {
-		filepath := webcrawler.GenerateFilePath(fileData.URL, destDir)
-		err := webcrawler.SaveToFile(filepath, fileData.Body)
-		if err != nil {
-			fmt.Printf("Error saving file: %v\n", err)
-		}
-	}
+    // Signal handler to gracefully shutdown the crawl manager
+    signalChan := make(chan os.Signal, 1)
+    signal.Notify(signalChan, os.Interrupt)
+    go func() {
+        <-signalChan
+        log.Println("Received interrupt signal, shutting down...")
+        c.Stop()
+    }()
 
-	fmt.Println("Crawling complete!")
+    // Wait for all URLs to be crawled
+    <-done
+
+    log.Println("All URLs crawled successfully")
+}
+
+func download(u string, destDir string) error {
+    // Create destination directory if it doesn't exist
+    if _, err := os.Stat(destDir); os.IsNotExist(err) {
+        if err := os.MkdirAll(destDir, 0755); err != nil {
+            return fmt.Errorf("failed to create destination directory: %v", err)
+        }
+    }
+
+    // Construct filename from URL
+    fileName := u[strings.LastIndex(url, "/")+1:]
+
+    // Download content from URL
+    resp, err := http.Get(u)
+    if err != nil {
+        return fmt.Errorf("failed to get URL: %v", err)
+    }
+    defer resp.Body.Close()
+
+    // Save content to file
+    filePath := filepath.Join(destDir, fileName)
+    file, err := os.Create(filePath)
+    if err != nil {
+        return fmt.Errorf("failed to create file: %v", err)
+    }
+    defer file.Close()
+
+    _, err = io.Copy(file, resp.Body)
+    if err != nil {
+        return fmt.Errorf("failed to save content to file: %v", err)
+    }
+
+    return nil
+}
+
+func getHrefs(reader io.Reader) ([]string, error) {
+    doc, err := html.Parse(reader)
+    if err != nil {
+        return nil, err
+    }
+
+    var hrefs []string
+    var visitNode func(node *html.Node)
+    visitNode = func(node *html.Node) {
+        if node.Type == html.ElementNode && node.Data == "a" {
+            for _, attr := range node.Attr {
+                if attr.Key == "href" {
+                    hrefs = append(hrefs, attr.Val)
+                }
+            }
+        }
+        for child := node.FirstChild; child != nil; child = child.NextSibling {
+            visitNode(child)
+        }
+    }
+    visitNode(doc)
+    return hrefs, nil
+}
+
+func processURLs(urls []string, destDir string) {
+    for _, url := range urls {
+        resp, err := http.Get(url)
+        if err != nil {
+            log.Println("Error retrieving", url, ":", err)
+            continue
+        }
+        defer resp.Body.Close()
+
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+            log.Println("Error reading response from", url, ":", err)
+            continue
+        }
+
+        hrefs := getHrefs(strings.NewReader(string(body)))
+        for _, href := range hrefs {
+            u, err := url.Parse(href)
+            if err != nil {
+                log.Println("Error parsing URL", href, ":", err)
+                continue
+            }
+            if u.IsAbs() {
+                processURLs([]string{u.String()}, destDir)
+            } else {
+                filename := filepath.Join(destDir, u.Path)
+                err := ioutil.WriteFile(filename, body, 0644)
+                if err != nil {
+                    log.Println("Error writing file", filename, ":", err)
+                    continue
+                }
+            }
+        }
+    }
+}
+
+func skipURL(url string, destination string) bool {
+    _, err := os.Stat(destination + "/" + url2filename(url))
+    if os.IsNotExist(err) {
+        return false
+    }
+    return true
+}
+
+func resume(dest string) (map[string]bool, error) {
+    downloaded := make(map[string]bool)
+
+    // Read the downloaded URLs from the destination directory
+    files, err := ioutil.ReadDir(dest)
+    if err != nil {
+        return nil, fmt.Errorf("failed to read directory: %v", err)
+    }
+
+    // Loop through each file and add its URL to the downloaded map
+    for _, file := range files {
+        url := file.Name()
+        downloaded[url] = true
+    }
+
+    return downloaded, nil
+}
+
+// interruptHandler is a function that handles the interrupt signal
+func interruptHandler(c chan os.Signal, done chan bool) {
+    // wait for the signal
+    <-c
+    // notify that we are done
+    done <- true
 }
 
